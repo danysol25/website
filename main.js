8c768bf47f97144a07613b9bfcf4c16ed9838499
DanySol <dceballos.dsc@gmail.com>
2021-02-08 00:06:09 +0100

    website with portfolio

---
+++ main.js
@@ -0,0 +1,10 @@
+// Validación de formulario de contacto
+	if(window.location.href.indexOf('contact') > -1){
+
+		$.validate({
+		    lang: 'es',
+		    errorMessagePosition: 'top',
+		    scrollToTopOnError: true
+		});
+
+	}
